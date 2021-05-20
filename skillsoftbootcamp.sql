7e3731558e9078b34cb5107cb698edc210df515b
Emmanuel Chebukati <echebukati@gmail.com>
2021-05-20 13:54:11 +0200

    Add files via upload

---
+++ skillsoftbootcamp.sql
@@ -0,0 +1,37 @@
+--
+-- Database: `skillsoftbootcamp`
+--
+CREATE DATABASE IF NOT EXISTS `skillsoftbootcamp` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `skillsoftbootcamp`;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'emmanuel', 'emmanuelpassword'),
+(2, 'james', 'jamespassword'),
+(3, 'stacy', 'stacypassword');
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+GRANT SELECT ON `skillsoftbootcamp`.* TO 'bootcamp'@'localhost';
