6353379eb46c6106c1152c4672658b47e36530f6
Emmanuel Chebukati <echebukati@gmail.com>
2026-04-20 14:06:31 +0300

    Modify users table structure and add primary key
    Updated the `users` table structure to include AUTO_INCREMENT for `id` and added a primary key constraint.

--- ssbootcamp.sql
+++ ssbootcamp.sql
@@ -1,40 +1,20 @@
---
--- Database: `ssbootcamp`
---
 CREATE DATABASE IF NOT EXISTS `ssbootcamp` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE `ssbootcamp`;
 
---
--- Table structure for table `users`
---
+DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `password` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
 (1, 'emmanuel', 'emmanuelpassword'),
 (2, 'james', 'jamespassword'),
 (3, 'stacy', 'stacypassword');
 
---
--- Indexes for table `users`
---
-
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
---
--- AUTO_INCREMENT for table `users`
---
-
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
 CREATE USER IF NOT EXISTS 'bootcamp'@'%' IDENTIFIED BY 'wC!viIkBek@6';
 GRANT SELECT ON `ssbootcamp`.* TO 'bootcamp'@'%';
+FLUSH PRIVILEGES;
